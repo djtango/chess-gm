84d96f4460dfdcfb570d5b767735b4d3e47b1c88
Deon Tan <deonjtan@gmail.com>
2018-09-21 11:04:57 +0100

    Add basic moves and captures

--- src/clj/chess_gm/pgn.clj
+++ src/clj/chess_gm/pgn.clj
@@ -1,4 +1,27 @@
 (ns chess-gm.pgn
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::basic-move any?)
+(s/def ::column-index (set "abcdefgh"))
+(s/def ::row-index    (set "12345678"))
+
+(s/def ::piece-name (set "KWBNRP"))
+
+(s/def ::position (s/cat :col ::column-index
+                         :row ::row-index))
+
+(s/def ::basic-move (s/cat :piece (s/? ::piece-name)
+                           :starting-column (s/? ::column-index)
+                           :position ::position))
+
+(s/def ::capture (s/cat :taker (s/or :pawn ::column-index
+                                     :piece ::piece-name)
+                        :x #{\x}
+                        :takee ::position))
+
+;; YAGNI
+;; (s/def ::pawn #{\P})
+;; (s/def ::rook #{\R})
+;; (s/def ::bishop #{\B})
+;; (s/def ::queen #{\Q})
+;; (s/def ::king #{\K})
+;; (s/def ::knight #{\N})
--- test/chess_gm/pgn_test.clj
+++ test/chess_gm/pgn_test.clj
@@ -5,6 +5,22 @@
             [chess-gm.pgn :as sut]))
 
 (deftest basic-SAN
-  (testing "a simple move should be valid"
-    (let [king-pawn-two-steps "e4"]
-      (is (s/valid? ::sut/basic-move (seq king-pawn-two-steps))))))
+  (testing "a simple vertical move should be valid"
+    (let [white-king-pawn-two-steps (seq "e4")]
+      (is (s/valid? ::sut/basic-move white-king-pawn-two-steps))))
+  (testing "piece names are supported"
+    (let [white-kingside-knight-moves-left (seq "Nf3")]
+      (is (s/valid? ::sut/basic-move white-kingside-knight-moves-left)))
+    (let [white-kingside-bishop-moves-left (seq "Bb5")]
+      (is (s/valid? ::sut/basic-move white-kingside-bishop-moves-left))))
+  (testing "should support moves that move across column"
+    (let [black-knight-protects-queen (seq "Nbd7")]
+      (is (s/valid? ::sut/basic-move black-knight-protects-queen)))))
+
+(deftest captures
+  (testing "should recognize 'x' as a capture"
+    (let [knight-captures-pawn (seq "Nxe4")]
+      (is (s/valid? ::sut/capture knight-captures-pawn))))
+  (testing "should also recognize pawn captures"
+    (let [pawn-captures-another-pawn (seq "axb5")]
+      (is (s/valid? ::sut/capture pawn-captures-another-pawn)))))
