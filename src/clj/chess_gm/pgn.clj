0a1282c32a2548051715a27d68388bd792b1140e
Deon Tan <deonjtan@gmail.com>
2018-12-05 18:03:30 +0000

    Consolidate types of move into ::move and test a canned game

--- src/clj/chess_gm/pgn.clj
+++ src/clj/chess_gm/pgn.clj
@@ -6,19 +6,25 @@
 
 (s/def ::piece-name (set "QKWBNRP"))
 
+(s/def ::castling (s/or :kingside #{(seq "O-O")}
+                        :queenside #{(seq "O-O-O")}))
 (s/def ::check #{\+})
 (s/def ::checkmate #{\#})
+(s/def ::draw #{(seq "1/2-1/2")})
+(s/def ::white-won #{(seq "1-0")})
+(s/def ::black-won #{(seq "0-1")})
+(s/def ::game-ongoing #{\*})
 
 (s/def ::position (s/cat :file ::column-index
                          :rank ::row-index))
 
+(s/def ::check-or-checkmate (s/alt :check (s/? ::check)
+                                   :checkmate (s/? ::checkmate)))
+
 (s/def ::basic-move (s/cat :piece (s/? ::piece-name)
                            :original-file (s/? ::column-index)
                            :original-rank (s/? ::row-index)
-                           :position ::position
-                           :pawn-promotion? (s/? ::pawn-promotion)
-                           :win? (s/alt :check (s/? ::check)
-                                        :checkmate (s/? ::checkmate))))
+                           :position ::position))
 (s/def ::pawn-promotion (s/cat :pawn-promotion #{\=}
                                :piece ::piece-name))
 
@@ -27,10 +33,11 @@
                         :original-file (s/? ::column-index)
                         :original-rank (s/? ::row-index)
                         :x #{\x}
-                        :takee ::position
-                        :pawn-promotion? (s/? ::pawn-promotion)
-                        :check (s/? ::check)
-                        :checkmate (s/? ::checkmate)))
+                        :takee ::position))
 
-(s/def ::castling (s/or :kingside #{(seq "O-O")}
-                        :queenside #{(seq "O-O-O")}))
+(s/def ::move (s/or :move (s/cat :piece-move (s/alt :basic-move ::basic-move
+                                                    :capture ::capture)
+                                 :?pawn-promotion (s/? ::pawn-promotion)
+                                 :?check-or-checkmate (s/? ::check-or-checkmate))
+                    :castling ::castling))
+
--- test/chess_gm/pgn_test.clj
+++ test/chess_gm/pgn_test.clj
@@ -26,19 +26,7 @@
          (seq "Rcb7")
          (seq "Nac3")
          (seq "Nbc3")
-         (seq "Ndc3")))
-  (testing "checking move with a '+' suffix is supported"
-    (is (s/valid? ::sut/basic-move (seq "Ra6+"))))
-  (testing "checkmating move with a '#' suffix is supported"
-    (is (s/valid? ::sut/basic-move (seq "Qg5#"))))
-  (testing "check and checkmate should not be simultaneously possible"
-    (is (not (s/valid? ::sut/basic-move (seq "Qg5#+"))))
-    (is (not (s/valid? ::sut/basic-move (seq "Qg5+#")))))
-  (testing "canned set of disambiguated moves should all be valid"
-    (are [s] (s/valid? ::sut/basic-move s)
-         (seq "Rbb7#")
-         (seq "Rab7+")
-         (seq "Rab7+"))))
+         (seq "Ndc3"))))
 
 (deftest captures
   (testing "should recognize 'x' as a capture"
@@ -51,15 +39,7 @@
     (are [s] (s/valid? ::sut/capture s)
          (seq "fxg5")
          (seq "hxg5")
-         (seq "hxg6")))
-  (testing "checking captures should work"
-    (are [s] (s/valid? ::sut/capture s)
-         (seq "Bxf7+")
-         (seq "Rxe1+")))
-  (testing "checkmating capture with a '#' suffix is supported"
-    (is (s/valid? ::sut/capture (seq "Qxg5#"))))
-  (testing "a disambiguated move that captures leading to a pawn promotion and check is supported"
-    (is (s/valid? ::sut/capture (seq "d7xc8=N+")))))
+         (seq "hxg6"))))
 
 (deftest castling
   (testing "kingside castling is supported"
@@ -69,12 +49,126 @@
 
 (deftest pawn-promotion
   (testing "an '=' denoting a pawn promotion should be supported"
-    (are [s] (s/valid? ::sut/basic-move s)
+    (are [s] (s/valid? ::sut/move s)
          (seq "e8=Q")
          (seq "d8=N#")
          (seq "d8=N+")))
   (testing "captures leading to a pawn promotion should be supported"
-    (are [s] (s/valid? ::sut/capture s)
+    (are [s] (s/valid? ::sut/move s)
          (seq "exd8=Q")
          (seq "dxc8=N+")
          (seq "dxc8=N#"))))
+
+(deftest check-or-checkmate
+  (testing "checking move with a '+' suffix is supported"
+    (is (s/valid? ::sut/move (seq "Ra6+"))))
+  (testing "checkmating move with a '#' suffix is supported"
+    (is (s/valid? ::sut/move (seq "Qg5#"))))
+  (testing "check and checkmate should not be simultaneously possible"
+    (is (not (s/valid? ::sut/move (seq "Qg5#+"))))
+    (is (not (s/valid? ::sut/move (seq "Qg5+#")))))
+  (testing "canned set of disambiguated moves should all be valid"
+    (are [s] (s/valid? ::sut/move s)
+         (seq "Rbb7#")
+         (seq "Rab7+")
+         (seq "Rab7+")))
+  (testing "checking captures should work"
+    (are [s] (s/valid? ::sut/move s)
+         (seq "Bxf7+")
+         (seq "Rxe1+")))
+  (testing "checkmating capture with a '#' suffix is supported"
+    (is (s/valid? ::sut/move (seq "Qxg5#"))))
+  (testing "a disambiguated move that captures leading to a pawn promotion and check is supported"
+    (is (s/valid? ::sut/move (seq "d7xc8=N+")))))
+
+(declare canned-game)
+(deftest check-a-canned-game
+  (testing "every move in a canned game should be valid"
+    (is (every? #(s/valid? ::sut/move (seq %)) canned-game))))
+
+(def canned-game
+  ["e4"
+   "e5"
+   "Nf3"
+   "Nc6"
+   "Bb5"
+   "a6"
+   "Ba4"
+   "Nf6"
+   "O-O"
+   "Be7"
+   "Re1"
+   "b5"
+   "Bb3"
+   "d6"
+   "c3"
+   "O-O"
+   "h3"
+   "Nb8"
+   "d4"
+   "Nbd7"
+   "c4"
+   "c6"
+   "cxb5"
+   "axb5"
+   "Nc3"
+   "Bb7"
+   "Bg5"
+   "b4"
+   "Nb1"
+   "h6"
+   "Bh4"
+   "c5"
+   "dxe5"
+   "Nxe4"
+   "Bxe7"
+   "Qxe7"
+   "exd6"
+   "Qf6"
+   "Nbd2"
+   "Nxd6"
+   "Nc4"
+   "Nxc4"
+   "Bxc4"
+   "Nb6"
+   "Ne5"
+   "Rae8"
+   "Bxf7+"
+   "Rxf7"
+   "Nxf7"
+   "Rxe1+"
+   "Qxe1"
+   "Kxf7"
+   "Qe3"
+   "Qg5"
+   "Qxg5"
+   "hxg5"
+   "b3"
+   "Ke6"
+   "a3"
+   "Kd6"
+   "axb4"
+   "cxb4"
+   "Ra5"
+   "Nd5"
+   "f3"
+   "Bc8"
+   "Kf2"
+   "Bf5"
+   "Ra7"
+   "g6"
+   "Ra6+"
+   "Kc5"
+   "Ke1"
+   "Nf4"
+   "g3"
+   "Nxh3"
+   "Kd2"
+   "Kb5"
+   "Rd6"
+   "Kc5"
+   "Ra6"
+   "Nf2"
+   "g4"
+   "Bd3"
+   "Re6"])
