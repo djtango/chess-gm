c5b62ebd0708ef145f1ec3316901a53e6a78d8b0
Deon Tan <deonjtan@gmail.com>
2018-12-05 18:02:51 +0000

    Spec check, castling and pawn promotion

--- src/clj/chess_gm/pgn.clj
+++ src/clj/chess_gm/pgn.clj
@@ -4,24 +4,33 @@
 (s/def ::column-index (set "abcdefgh"))
 (s/def ::row-index    (set "12345678"))
 
-(s/def ::piece-name (set "KWBNRP"))
+(s/def ::piece-name (set "QKWBNRP"))
 
-(s/def ::position (s/cat :col ::column-index
-                         :row ::row-index))
+(s/def ::check #{\+})
+(s/def ::checkmate #{\#})
+
+(s/def ::position (s/cat :file ::column-index
+                         :rank ::row-index))
 
 (s/def ::basic-move (s/cat :piece (s/? ::piece-name)
-                           :starting-column (s/? ::column-index)
-                           :position ::position))
+                           :original-file (s/? ::column-index)
+                           :original-rank (s/? ::row-index)
+                           :position ::position
+                           :pawn-promotion? (s/? ::pawn-promotion)
+                           :win? (s/alt :check (s/? ::check)
+                                        :checkmate (s/? ::checkmate))))
+(s/def ::pawn-promotion (s/cat :pawn-promotion #{\=}
+                               :piece ::piece-name))
 
 (s/def ::capture (s/cat :taker (s/or :pawn ::column-index
                                      :piece ::piece-name)
+                        :original-file (s/? ::column-index)
+                        :original-rank (s/? ::row-index)
                         :x #{\x}
-                        :takee ::position))
+                        :takee ::position
+                        :pawn-promotion? (s/? ::pawn-promotion)
+                        :check (s/? ::check)
+                        :checkmate (s/? ::checkmate)))
 
-;; YAGNI
-;; (s/def ::pawn #{\P})
-;; (s/def ::rook #{\R})
-;; (s/def ::bishop #{\B})
-;; (s/def ::queen #{\Q})
-;; (s/def ::king #{\K})
-;; (s/def ::knight #{\N})
+(s/def ::castling (s/or :kingside #{(seq "O-O")}
+                        :queenside #{(seq "O-O-O")}))
--- test/chess_gm/pgn_test.clj
+++ test/chess_gm/pgn_test.clj
@@ -15,7 +15,30 @@
       (is (s/valid? ::sut/basic-move white-kingside-bishop-moves-left))))
   (testing "should support moves that move across column"
     (let [black-knight-protects-queen (seq "Nbd7")]
-      (is (s/valid? ::sut/basic-move black-knight-protects-queen)))))
+      (is (s/valid? ::sut/basic-move black-knight-protects-queen))))
+  (testing "disambiguating moves is supported"
+    (let [queen-moves-from-h8-to-g7 (seq "Qh8g7")]
+      (is (s/valid? ::sut/basic-move queen-moves-from-h8-to-g7))))
+  (testing "canned set of disambiguated moves should all be valid"
+    (are [s] (s/valid? ::sut/basic-move s)
+         (seq "Rbb7")
+         (seq "Rab7")
+         (seq "Rcb7")
+         (seq "Nac3")
+         (seq "Nbc3")
+         (seq "Ndc3")))
+  (testing "checking move with a '+' suffix is supported"
+    (is (s/valid? ::sut/basic-move (seq "Ra6+"))))
+  (testing "checkmating move with a '#' suffix is supported"
+    (is (s/valid? ::sut/basic-move (seq "Qg5#"))))
+  (testing "check and checkmate should not be simultaneously possible"
+    (is (not (s/valid? ::sut/basic-move (seq "Qg5#+"))))
+    (is (not (s/valid? ::sut/basic-move (seq "Qg5+#")))))
+  (testing "canned set of disambiguated moves should all be valid"
+    (are [s] (s/valid? ::sut/basic-move s)
+         (seq "Rbb7#")
+         (seq "Rab7+")
+         (seq "Rab7+"))))
 
 (deftest captures
   (testing "should recognize 'x' as a capture"
@@ -23,4 +46,35 @@
       (is (s/valid? ::sut/capture knight-captures-pawn))))
   (testing "should also recognize pawn captures"
     (let [pawn-captures-another-pawn (seq "axb5")]
-      (is (s/valid? ::sut/capture pawn-captures-another-pawn)))))
+      (is (s/valid? ::sut/capture pawn-captures-another-pawn))))
+  (testing "disambiguated captures should work"
+    (are [s] (s/valid? ::sut/capture s)
+         (seq "fxg5")
+         (seq "hxg5")
+         (seq "hxg6")))
+  (testing "checking captures should work"
+    (are [s] (s/valid? ::sut/capture s)
+         (seq "Bxf7+")
+         (seq "Rxe1+")))
+  (testing "checkmating capture with a '#' suffix is supported"
+    (is (s/valid? ::sut/capture (seq "Qxg5#"))))
+  (testing "a disambiguated move that captures leading to a pawn promotion and check is supported"
+    (is (s/valid? ::sut/capture (seq "d7xc8=N+")))))
+
+(deftest castling
+  (testing "kingside castling is supported"
+    (is (s/valid? ::sut/castling (seq "O-O"))))
+  (testing "queenside castling is supported"
+    (is (s/valid? ::sut/castling (seq "O-O-O")))))
+
+(deftest pawn-promotion
+  (testing "an '=' denoting a pawn promotion should be supported"
+    (are [s] (s/valid? ::sut/basic-move s)
+         (seq "e8=Q")
+         (seq "d8=N#")
+         (seq "d8=N+")))
+  (testing "captures leading to a pawn promotion should be supported"
+    (are [s] (s/valid? ::sut/capture s)
+         (seq "exd8=Q")
+         (seq "dxc8=N+")
+         (seq "dxc8=N#"))))
